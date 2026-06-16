51b5edfef7e2a2976db0fd4e4fb6db4275d79b16
StevenAnchundia <erick.anchundia01@cu.ucsg.edu.ec>
2026-06-16 11:24:02 -0500

    Add Cliente

--- modelo/cliente.py
+++ modelo/cliente.py
@@ -1,1 +1,9 @@
 
+class Cliente:
+    def __init__(self, nombre, cedula, telefono):
+        self.nombre = nombre
+        self.cedula = cedula
+        self.telefono = telefono
+
+    def __str__(self):
+        return f"{self.nombre} | Cedula: {self.cedula} | Telefono: {self.telefono}"
