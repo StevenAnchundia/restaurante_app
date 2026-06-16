337bb6955853740a64f316b69e33fbca561c5db1
StevenAnchundia <erick.anchundia01@cu.ucsg.edu.ec>
2026-06-16 11:23:24 -0500

    Add Producto

--- modelo/producto.py
+++ modelo/producto.py
@@ -1,1 +1,9 @@
 
+class Producto:
+    def __init__(self, nombre, precio, categoria):
+        self.nombre = nombre
+        self.precio = precio
+        self.categoria = categoria
+
+    def __str__(self):
+        return f"{self.nombre} | Categoría: {self.categoria} | Precio: ${self.precio:.2f}"
